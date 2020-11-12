d2b91e21466d3eb9b3a88274c45f1b0941004de0
tanviranik004 <tanvir.anik.009@gmail.com>
2020-11-13 01:27:29 +0600

    bug fixed

---
+++ src/components/Product/Product.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Product = (props) => {
+    
+    //const {name, id}= props.product;
+    const {addToCart, product} = props;
+
+    return (
+        <div style={{border: '1px solid tomato'}}>
+            <h5>{product.name}</h5>
+            <button 
+            onClick={()=>addToCart(product.id,product.name)}
+            >add to cart</button>
+        </div>
+    );
+};
+
+export default Product;
---
+++ src/redux/reducers/cartReducers.js
@@ -0,0 +1,40 @@
+const { ADD_TO_CART, REMOVE_FROM_CART } = require("../actions/cartActions");
+
+const initialState = {
+    cart: [],
+    products: [
+
+        {name: 'Lenovo Laptop', id: 1},
+        {name: 'Asus Laptop', id: 2},
+        {name: 'DELL Laptop', id: 3},
+        {name: 'Tushiba Laptop', id: 4},
+        {name: 'Walton Laptop', id: 5},
+        {name: 'APPLE Laptop', id: 6},
+        {name: 'Xaomi Laptop', id: 7},
+    ]
+}
+
+const cartReducers = (state=initialState,action) =>{
+    switch(action.type){
+        case ADD_TO_CART:
+           // console.log(action);
+            //const newId=action.id;
+            const newItem = {
+                productId: action.id, 
+                name: action.name, 
+                cartId: state.cart.length + 1
+            }
+            const newCart = [...state.cart ,newItem];
+            return{...state, cart: newCart};
+
+            case REMOVE_FROM_CART:
+                 const id=action.id;
+                 const remainingCart=state.cart.filter(item=>item.cartId!==id)
+                 return{...state,cart: remainingCart}
+
+        default:
+            return state;
+    }
+}
+
+export default cartReducers;
